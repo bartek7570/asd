f6a727d7bae24111bacbd7f6160b53951359382d
bartek7570 <bartek7570@wp.pl>
2015-06-14 12:14:04 +0200

    commitmessage

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhost.WebService1 myMathService = new localhost.WebService1();
+            Console.Write("2 + 4 =", myMathService.Add(2, 4));
+        }
+    }
+}
